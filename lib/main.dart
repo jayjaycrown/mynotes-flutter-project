b08b563e3cbfea1e5c84267b17e7f1955980339a
olajide <olajidecrown@gmail.com>
2023-04-03 23:37:21 +0100

    Added logout button and redirect to login page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 // ignore_for_file: avoid_print
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/firebase_options.dart';
@@ -35,13 +36,25 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // if (user!.emailVerified) {
-            // } else {
-            //   return const VerifyEmailView();
-            // }
-            return const LoginView();
-          // return const Text('Done');
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('Email verified');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return Scaffold(
+                appBar: AppBar(
+                  title: const Text('Home'),
+                ),
+                body: Center(
+                  child: Column(
+                    children: const [Text('Done')],
+                  ),
+                ));
           default:
             return const CircularProgressIndicator();
         }
